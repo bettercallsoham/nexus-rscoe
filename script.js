0dcb087489478405ef3d999eb6b63ec496cd4498
Soham <rimatip472@operades.com>
2025-06-26 21:00:16 +0530

    fixed the layout and remove unnecessary files

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    const item = document.getElementById('item')
+
+    if (window.scrollY > 50) { 
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+document.querySelector("#contactForm").addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    success();         
+});
+
+function success() {
+    document.getElementById("successText").innerHTML = " <b> Successfully submitted! ✅ </b>";
+}
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
