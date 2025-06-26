9f7723406a2489025408ae96673e2777aa7692a7
Soham <rimatip472@operades.com>
2025-06-26 21:24:53 +0530

    moved particles.json into assets folder

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ function success() {
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
+  console.log('particles loaded!');
 });
